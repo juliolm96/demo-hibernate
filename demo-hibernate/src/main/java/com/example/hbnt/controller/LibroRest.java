cc72a475bc2649df8767461422eaa0eea1e56a48
Julio Leon <julio_lm96@outlook.com>
2019-10-26 12:51:55 -0500

    Adición de nuevos servicios para obtener autores.

--- demo-hibernate/src/main/java/com/example/hbnt/controller/LibroRest.java
+++ demo-hibernate/src/main/java/com/example/hbnt/controller/LibroRest.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hbnt.model.LibroM;
@@ -24,10 +26,32 @@ public class LibroRest {
 	LibroService libroService;
 	
 	@GetMapping
-	public ResponseEntity<List<LibroM>> obtenerLibros() {
+	public ResponseEntity<List<LibroM>> getLibros() {
 		try {
 			return new ResponseEntity<>(libroService.findAll(), HttpStatus.OK);
 		} catch (Exception e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<LibroM> find(@PathVariable int id) {
+		try {
+			return new ResponseEntity<>(libroService.findById(id), HttpStatus.OK);
+		} catch(Exception e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/titulo")
+	public ResponseEntity<List<LibroM>> findByTitulo(@RequestParam String titulo) {
+		try {
+			return new ResponseEntity<>(libroService.findByTitulo(titulo), HttpStatus.OK);
+		} catch(Exception e) {
 			e.printStackTrace();
 			log.error(e.getMessage());
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
